1522783fd17d24b4ff944ac0f7f21b57548e8878
mirkovic.uk <mirkovic.uk@gmail.com>
2024-06-26 14:02:54 +0100

    test lambda_users

--- backend/lambda_users.py
+++ backend/lambda_users.py
@@ -1,4 +1,20 @@
+import json
+import boto3
+import time
+from datetime import datetime as dt
 
 
 def lambda_handler(event, context):
-    pass
+    dynamodb = boto3.resource('dynamodb')
+    users_table = dynamodb.Table('users')
+
+    body = json.loads(event['body'])
+    user_name = body['user_name']
+    password = body ['password']
+    
+
+    users_table.put_item(Item={
+        'user_name' : user_name,
+        'password' : password,
+        'last_updated_time' : dt.now().strftime("%d/%m/%Y, %H:%M:%S")
+    })
--- tests/test_lambda_users.py
+++ tests/test_lambda_users.py
@@ -1,2 +1,69 @@
 from backend.lambda_users import lambda_handler
 
+from moto import mock_aws
+
+import boto3
+import pytest
+import os
+import json
+
+
+
+@mock_aws
+def test_lambda():
+    with open('backend/api_gateway_event.txt', 'r') as f:
+        event = json.loads(f.read())
+    
+    body = '{"user_name":"new_user1","password":"pasword12345"}'
+    http_method = 'POST'
+    event['body'], event['httpMethod'] = body, http_method
+
+    AttributeDefinitions=[
+        {
+            'AttributeName': 'user_name',
+            'AttributeType': 'S',
+        },
+        {
+            'AttributeName': 'password',
+            'AttributeType': 'S',
+        },
+    ]
+    KeySchema=[
+        {
+            'AttributeName': 'user_name',
+            'KeyType': 'HASH',
+        },
+        {
+            'AttributeName': 'password',
+            'KeyType': 'HASH',
+        },
+    ]
+    ProvisionedThroughput={
+        'ReadCapacityUnits': 5,
+        'WriteCapacityUnits': 5,
+    }
+    TableName='users'
+
+    client = boto3.client('dynamodb')   
+    client.create_table(AttributeDefinitions=AttributeDefinitions,
+                        KeySchema = KeySchema,
+                        ProvisionedThroughput = ProvisionedThroughput,
+                        TableName=TableName)
+    
+    response = lambda_handler(event, context=None)
+
+    response = client.get_item(
+                Key={
+                    'user_name': {
+                        'S': 'new_user1',
+                    },
+                },
+                TableName='Music',
+                )
+
+    print(response)
+
+    
+
+
+
