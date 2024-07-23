80ed1f8183725701cce7d971083a505a88236434
mirkovic.uk <mirkovic.uk@gmail.com>
2024-07-23 10:54:32 +0100

    usernameExistsExceptionHandler added popover snippet with suggestions

--- client/__tests__/signUp.test.js
+++ client/__tests__/signUp.test.js
@@ -1,10 +1,10 @@
+
 import { mockClient } from "aws-sdk-client-mock";
 import { 
   SignUpCommand,
   CognitoIdentityProviderClient
 } from "@aws-sdk/client-cognito-identity-provider";
-import { signUp } from "../src/signUp.js"
-
+import { signUp, cognitoSignUpHandler } from "../src/signUp.js"
 
 
 const cognitoMock = mockClient(CognitoIdentityProviderClient)
@@ -13,16 +13,16 @@ beforeEach(() => {
 });
 
 describe('signUp()', () => {
-  it('signeUp is mocked', async () => {
-
+  it('signUp is mocked', async () => {
+    
     const cognitoResponse = {
-      UserConfirmed: false, // required
-      CodeDeliveryDetails: { // CodeDeliveryDetailsType
+      UserConfirmed: false, 
+      CodeDeliveryDetails: { 
         Destination: "STRING_VALUE",
         DeliveryMedium: "EMAIL",
         AttributeName: "email",
       },
-      UserSub: "123456", // required
+      UserSub: "123456",
       $metadata:{
         httpStatusCode : 200}
       }
@@ -34,9 +34,27 @@ describe('signUp()', () => {
       password:'test',
       email:'test'
     }
-    const result = await signUp(input)
-    expect(result).toBe(cognitoResponse)
+    const result =  await signUp(input)
+    expect(result).toEqual(cognitoResponse)
   })
+  
+  it('signUp doesnot mutate input', async()=>{
+    const cognitoResponse = {}
+    cognitoMock.on(SignUpCommand).resolves(cognitoResponse)
 
+    const input = {
+      clientId:'test',
+      username:'test',
+      password:'test',
+      email:'test'
+    }
+    const result =  await signUp(input)
+    expect(input).toEqual({
+      clientId:'test',
+      username:'test',
+      password:'test',
+      email:'test'
+    })
+  })
 })
 
--- client/src/signUp.js
+++ client/src/signUp.js
@@ -1,3 +1,20 @@
+function listeners(){
+        //signIn checkbox eventListener
+        document.querySelector('input[type=checkbox]').
+        addEventListener('change', ()=>{
+            showPassword()
+        })
+
+        // on submit form build JS obj and send it to cognitoSignUpHandler
+        document.querySelector('#signUpForm').addEventListener('submit',(e)=>{
+        e.preventDefault();
+        const formDataObj = Object.fromEntries(new FormData(e.target))
+            cognitoSignUpHandler(formDataObj)
+        })
+}
+// uncoment b4 npm run build command
+// listeners()
+
 function showPassword(){
     const x = document.getElementById("password");
     if (x.type === "password") {
@@ -7,8 +24,6 @@ function showPassword(){
     }
 }
 
-
-
 /////////////////////////////////////////////////////////////////////////////////////
 //####### Cognito signUp #########################################################
 ///////////////////////////////////////////////////////////////////////////////////
@@ -16,6 +31,7 @@ function showPassword(){
 import {
     SignUpCommand,
     CognitoIdentityProviderClient,
+    UsernameExistsException,
 } from "@aws-sdk/client-cognito-identity-provider";
 
 const signUp = ({clientId, username, password, email, region='eu-west-2'}) => {
@@ -32,9 +48,44 @@ const signUp = ({clientId, username, password, email, region='eu-west-2'}) => {
 };  
 const cognitoSignUpHandler = async (data)=>{
     data.clientId = window.CONFIG.clientId
-    const response = await signUp(data)
-    console.log(response)
-    console.log(response['$metadata'].httpStatusCode === 200)
+    try {
+        const response = await signUp(data)
+        if(response['$metadata'].httpStatusCode === 200){
+            successfulSignUp()
+        }
+    } catch (error) {
+        if(error instanceof UsernameExistsException){
+           usernameExistsExceptionHandler()
+        }else{
+            throw error
+        }
+    }
 }
 
-export { signUp };
+function usernameExistsExceptionHandler(){
+    //color username text in red insert popover with some sugestions
+    console.log('write into div user name exist, give recomentadition')
+    const usernameSuggestions = 'IDK'
+    const snippet = `<a tabindex="0" class="btn btn-sm btn-transparent" 
+    role="button" data-bs-toggle="popover" data-bs-trigger="focus" 
+    title="SUGGESTIONS" data-bs-content="${usernameSuggestions}">
+    Click me for suggestions:</a>`
+    document.getElementById('userNameHelp').innerHTML = snippet
+}
+
+//to be called when user is added to cognito 
+function successfulSignUp(){
+    const snippet = `<div class="alert alert-success alert-dismissible fade show" role="alert">
+  <h4 class="alert-heading">Well done!</h4>
+  <p>A verification email has been sent to your email address</p>
+  <hr>
+  <p class="mb-0">Please click on the link that been sent to your email account to verify your email and complete registration process.</p>
+  <button onclick="location.href='index.html';" type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+  </div>`
+
+    document.getElementById ('mainDiv').innerHTML = snippet
+
+  
+}
+
+export { signUp, cognitoSignUpHandler, showPassword };
